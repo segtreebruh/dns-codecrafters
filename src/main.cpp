51139d0b28b8d2993bc955d16ce964d97ae04989
Cuong Dang <dvucuong2005@gmail.com>
2026-03-17 23:34:56 -0400

    parse compressed questions

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,10 @@ struct DNSQuestion {
     std::vector<uint8_t> qname;
     uint16_t type;
     uint16_t cls;
+    bool isCompressed;
+    size_t sz;
 
-    DNSQuestion(): qname(std::vector<uint8_t>()), type(0), cls(0) {}
-    size_t sz() { return qname.size() + 2 * 2; }
+    DNSQuestion(): qname(std::vector<uint8_t>()), type(0), cls(0), isCompressed(false), sz(0) {}
 };
 
 struct DNSAnswer {
@@ -61,50 +62,100 @@ DNSHeader parseHeader(DNSHeader& reqHeader) {
     uint16_t rcode = (opcode == 0) ? 0 : 4;
 
     header.flags |= htons((qr << 15) | (opcode << 11) | (aa << 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | (rcode));
-    header.qdcount = htons(1);
-    header.ancount = htons(1);
+    header.qdcount = reqHeader.qdcount;
+    header.ancount = reqHeader.qdcount;
     header.nscount = htons(0);
     header.arcount = htons(0);
 
     return header;
 }
 
-DNSQuestion parseQuestion(char* response, const char* buffer, size_t questionOffset) {
-    // nextQuestionBuffer: buffer after all previous headers + questions
+DNSQuestion parseQuestion(const char* buffer, size_t readOffset) {
+    // readOffset: offset from beginning of buffer of current question
     DNSQuestion question;
     
-    const char* ptr = buffer;
+    const char* ptr = buffer + readOffset;
     
     while (true) {
         // face 11 bit: go back 
-        // 0xc0: 1100 0000 0000 0000
-        if ((*(uint16_t*)ptr & 0xc0) == 0xc0) {
-            uint16_t offset = (*(uint16_t*)ptr & 0x3f);
+        // 0xc0: 1100 0000
+        if ((*ptr & 0xc0) == 0xc0) {
+            // copy 16 bit from ptr to raw
+            uint16_t raw;
+            memcpy(&raw, ptr, sizeof(uint16_t));
+            // 0011 1111 1111 1111
+            // delete first two bits
+            uint16_t offset = ntohs(raw) & 0x3FFF;
             ptr = buffer + offset;
+            question.isCompressed = true;
+            question.sz += 2;
             continue;
         }
 
         question.qname.push_back(*ptr);
+        if (!question.isCompressed) question.sz += 1;
         ptr++;
         if (question.qname.back() == 0x00) break;
     }
 
     question.type = htons(1);
     question.cls = htons(1);
+    question.sz += 4;
 
     return question;
 }
 
-DNSAnswer parseAnswer(char* response, DNSQuestion& question) {
+DNSAnswer parseAnswer(DNSQuestion& question) {
     DNSAnswer answer(question);
 
-    answer.ttl = htons(60);
+    answer.ttl = htonl(60);
     answer.len = htons(4);
-    answer.data = htons(0x08080808); // 8.8.8.8
+    answer.data = htonl(0x08080808); // 8.8.8.8
 
     return answer;
 }
 
+size_t createResponse(char* buffer, char* response) {
+    DNSHeader* reqHeader = reinterpret_cast<DNSHeader*>(buffer);
+    DNSHeader header = parseHeader(*reqHeader);
+    int numQuestions = ntohs(header.qdcount);
+
+    std::vector<DNSQuestion> questions;
+    size_t questionsOffset = sizeof(DNSHeader);
+    for (int i = 0; i < numQuestions; i++) {
+        DNSQuestion question = parseQuestion(buffer, questionsOffset);
+        questions.push_back(question);
+        questionsOffset += question.sz;
+    }
+
+    std::vector<DNSAnswer> answers;
+    for (auto& q: questions) {
+        DNSAnswer answer = parseAnswer(q);
+        answers.push_back(answer);
+    }
+
+    char* ptr = response;
+    memcpy(ptr, &header, sizeof(DNSHeader));
+    ptr += sizeof(DNSHeader);
+
+    for (auto& q: questions) {
+        // uncompressed qname
+        memcpy(ptr, q.qname.data(), q.qname.size()); ptr += q.qname.size();
+        memcpy(ptr, &q.type, sizeof(q.type)); ptr += sizeof(q.type);
+        memcpy(ptr, &q.cls, sizeof(q.cls)); ptr += sizeof(q.cls);
+    }
+
+    for (auto& a: answers) {
+        memcpy(ptr, a.qname.data(), a.qname.size()); ptr += a.qname.size();
+        memcpy(ptr, &a.type, sizeof(a.type)); ptr += sizeof(a.type);
+        memcpy(ptr, &a.cls, sizeof(a.cls)); ptr += sizeof(a.cls);
+        memcpy(ptr, &a.ttl, sizeof(a.ttl)); ptr += sizeof(a.ttl);
+        memcpy(ptr, &a.len, sizeof(a.len)); ptr += sizeof(a.len);
+        memcpy(ptr, &a.data, sizeof(a.data)); ptr += sizeof(a.data);
+    }
+    
+    return ptr - response;
+}
 
 int main() {
     // Flush after every std::cout / std::cerr
@@ -170,16 +221,7 @@ int main() {
         // const DNSQuery* dnsQuery = reinterpret_cast<DNSQuery*>(buffer);
         // std::cerr << "query" << endl;
 
-        DNSHeader* reqHeader = reinterpret_cast<DNSHeader*>(buffer);
-        DNSHeader header = parseHeader(*reqHeader);
-        DNSQuestion question = parseQuestion(response, buffer);
-
-        // add headerLen to buffer to make buffer starts after DNSHeader
-        size_t questionLen = parseQuestion(response + headerLen, buffer + headerLen);
-        const char* qRes = response + headerLen;
-        size_t answerLen = parseAnswer(response + headerLen + questionLen, qRes, questionLen);
-
-        size_t responseLen = headerLen + questionLen + answerLen;
+        size_t responseLen = createResponse(buffer, response);
 
         if (sendto(udpSocket, response, responseLen, 0, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
             perror("Failed to send response");
